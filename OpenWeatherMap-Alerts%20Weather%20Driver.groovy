b673c8e1ccb3a0f1848a353e3f29e0d7b8ef5751
Scottma61 <Matthew@5-Scotts.com>
2026-07-27 20:06:16 +0000

    Update OpenWeatherMap-Alerts%20Weather%20Driver.groovy

--- OpenWeatherMap-Alerts%20Weather%20Driver.groovy
+++ OpenWeatherMap-Alerts%20Weather%20Driver.groovy
@@ -149,7 +149,7 @@ static String version()	{  return '0.7.3'  }
 import groovy.transform.Field
 
 metadata {
-	definition (name: 'OpenWeatherMap-Alerts Weather Driver-DEPRECATED',
+	definition (name: 'OpenWeatherMap-Alerts Weather Driver',
 		namespace: 'Matthew',
 		author: 'Scottma61',
 		importUrl: 'https://raw.githubusercontent.com/HubitatCommunity/OpenWeatherMap-Alerts-Weather-Driver/master/OpenWeatherMap-Alerts%2520Weather%2520Driver.groovy') {
